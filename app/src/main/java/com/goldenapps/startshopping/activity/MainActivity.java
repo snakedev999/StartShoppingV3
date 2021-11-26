294461767406e8be2e7e52e3ec6001b3bdf2d9d1
michaelvenegas <50221114+michaelvenegas@users.noreply.github.com>
2021-11-26 19:12:55 -0300

    realice la implementacion del maps , 2 botones uno para mostrar la ubicacion de la tienda, y otro para ver la locazacion mia

--- app/src/main/java/com/goldenapps/startshopping/activity/MainActivity.java
+++ app/src/main/java/com/goldenapps/startshopping/activity/MainActivity.java
@@ -2,12 +2,16 @@ package com.goldenapps.startshopping.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.goldenapps.startshopping.MapMiLocalizacion;
+import com.goldenapps.startshopping.MapsActivity1;
 import com.goldenapps.startshopping.R;
 import com.goldenapps.startshopping.adapterss.RecyclerAdapter;
 import com.goldenapps.startshopping.model.ItemList;
@@ -16,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
+    Button btn_tipo,btn_ubicacion,btn_sitio;//definimos los 3 botones que tenemos para nuestro mapa
     private RecyclerView rvLista;
     private SearchView svSearch;
     private RecyclerAdapter adapter;
@@ -28,6 +33,22 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.fragment_category);
 
         initViews();
+        btn_sitio=(Button)findViewById(R.id.btn_sitio);// creamos un casting de tipo button y lo buscamos mediante su ID
+        btn_ubicacion=(Button)findViewById(R.id.btn_ubicacion);
+        btn_sitio.setOnClickListener(new View.OnClickListener() {//programamos el boton btn_sitio con el fin de que nos proporcione un mapa
+
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(), MapsActivity1.class);
+                startActivity(intent);
+            }
+            //Metodo de Localizacion
+            public void Milocalizacion(View v){
+            Intent intent = new Intent(getApplicationContext(), MapMiLocalizacion.class);
+            startActivity(intent);
+            }
+        });
+
     }
 
     private void initViews(){
