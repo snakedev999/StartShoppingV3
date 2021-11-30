8454544f3c6bf0d3e2f463471c5b2162fe60c188
GitDroid999 <90290099+GitDroid999@users.noreply.github.com>
2021-11-30 18:44:23 -0300

    71

--- app/src/main/java/com/goldenapps/startshopping/ui/productoDetalle/DetalleActivity.java
+++ app/src/main/java/com/goldenapps/startshopping/ui/productoDetalle/DetalleActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,7 @@ import com.goldenapps.startshopping.R;
 import com.goldenapps.startshopping.carrito.CarritoActivity;
 import com.goldenapps.startshopping.model.ModelCarrito;
 import com.goldenapps.startshopping.model.ModelItemCarrito;
+import com.goldenapps.startshopping.model.ModelProducto;
 import com.goldenapps.startshopping.model.ModelPuntajeProducto;
 import com.goldenapps.startshopping.ui.menu.MenuActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -41,7 +43,6 @@ public class DetalleActivity extends AppCompatActivity {
     private RatingBar ratingBar;
     private EditText tallaDeseda;
     private FloatingActionButton fl_carrito;
-    double ratingTotal;
     private DbHelper helper;
     private SQLiteDatabase db;
     private String idUser = "";
@@ -50,11 +51,20 @@ public class DetalleActivity extends AppCompatActivity {
     private Button mProducto,rProducto;
     private TextView cantidadDeseada;
     int cantidadDeseadaInt = 1;
+    private String v;
     int cantidadDeseadaEnviar;
+    private String idProductoPuntajeQryUsuario;
+    private String idUsuarioPuntajeQryUsuario;
+    private String idProductoPuntajeQryProducto,idUsuarioPuntajeQryProducto;
+    private String idPuntajeQryProducto;
+    private String idPuntajeQryUsuario;
+    private boolean bandera;
+    private ArrayList<ModelPuntajeProducto> listPuntajeProductoUsuario;
     private ArrayList<ModelPuntajeProducto> listPuntajeProducto;
+    private ArrayList<ModelPuntajeProducto> listPuntaje;
     private DatabaseReference databaseReferenceCarrito;
     private DatabaseReference databaseReferenceItemCarrito;
-    private DatabaseReference databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
+    private DatabaseReference databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference();
 
     String idPro,name, descrip,image,talla;
     int cantidad;
@@ -65,7 +75,9 @@ public class DetalleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_detalle);
+        listPuntajeProductoUsuario = new ArrayList<>();
         listPuntajeProducto = new ArrayList<>();
+        listPuntaje = new ArrayList<>();
         consultaUsuario(1);
 
         idPro = "";
@@ -178,29 +190,31 @@ public class DetalleActivity extends AppCompatActivity {
 
         Glide.with(getApplicationContext()).load(image).into(img);
 
-
+        databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
         databaseReferencePuntaje.orderByChild("idProducto").equalTo(idPro).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if(snapshot.exists()) {
-                    listPuntajeProducto.removeAll(listPuntajeProducto);
+                    double ratingTotal = 0.0;
+                    double rnting;
+                    listPuntaje.removeAll(listPuntaje);
                     for(DataSnapshot oPuntaje : snapshot.getChildren()){
                         ModelPuntajeProducto puntajeProducto = oPuntaje.getValue(ModelPuntajeProducto.class);
                         String idPuntaje = oPuntaje.getKey();
                         String idProducto = puntajeProducto.getIdProducto();
                         String idUsuario = puntajeProducto.getIdUsuario();
                         double puntaje = puntajeProducto.getPuntajeProducto();
-                        listPuntajeProducto.add(new ModelPuntajeProducto(idPuntaje,idProducto,idUsuario,puntaje));
+                        listPuntaje.add(new ModelPuntajeProducto(idPuntaje,idProducto,idUsuario,puntaje));
                     }
-
-                    for (ModelPuntajeProducto modelPuntajeProducto : listPuntajeProducto){
+                    for (ModelPuntajeProducto modelPuntajeProducto : listPuntaje){
                         if (modelPuntajeProducto.getIdProducto().equals(idPro)) {
-                            ratingTotal = ratingTotal + modelPuntajeProducto.getPuntajeProducto();
+                            rnting = modelPuntajeProducto.getPuntajeProducto();
+                            ratingTotal = ratingTotal + rnting;
                         }
                     }
-                    int sizeArray = listPuntajeProducto.size();
-                    double rating = ratingTotal /sizeArray;
-                    String s = String.valueOf(rating);
+                    int sizeArray = listPuntaje.size();
+                    double rating = ratingTotal / sizeArray;
+                    String s = Double.toString(rating);
                     textRating.setText(s);
                 }
             }
@@ -215,51 +229,82 @@ public class DetalleActivity extends AppCompatActivity {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
                 if(!getIdUser().equals("")){
-                    listPuntajeProducto.removeAll(listPuntajeProducto);
+                    listPuntajeProductoUsuario.removeAll(listPuntajeProductoUsuario);
+                    databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
                     databaseReferencePuntaje.orderByChild("idUsuario").equalTo(getIdUser()).addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
                             if(snapshot.exists()) {
-                                for(DataSnapshot oPuntaje : snapshot.getChildren()){
+                                for (DataSnapshot oPuntaje : snapshot.getChildren()) {
                                     ModelPuntajeProducto puntajeProducto = oPuntaje.getValue(ModelPuntajeProducto.class);
                                     String idPuntaje = oPuntaje.getKey();
                                     String idProducto = puntajeProducto.getIdProducto();
                                     String idUsuario = puntajeProducto.getIdUsuario();
                                     double puntaje = puntajeProducto.getPuntajeProducto();
-                                    listPuntajeProducto.add(new ModelPuntajeProducto(idPuntaje,idProducto,idUsuario,puntaje));
+                                    listPuntajeProductoUsuario.add(new ModelPuntajeProducto(idPuntaje, idProducto, idUsuario, puntaje));
                                 }
 
-                                for (ModelPuntajeProducto modelPuntajeProducto : listPuntajeProducto){
-                                    if (modelPuntajeProducto.getIdProducto().equals(idPro) && modelPuntajeProducto.getIdUsuario().equals(idUser)) {
-                                        databaseReferencePuntaje.orderByChild("idProducto").equalTo(idPro).addValueEventListener(new ValueEventListener() {
-                                            @Override
-                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                                if(snapshot.exists()){
-                                                    String idPro = modelPuntajeProducto.getIdProducto();
-                                                    String idUser = modelPuntajeProducto.getIdUsuario();
-                                                    double puntaje = ratingBar.getRating();
+                                for (ModelPuntajeProducto modelPuntajeProducto : listPuntajeProductoUsuario){
+                                    if (!modelPuntajeProducto.getIdProducto().equals(idPro) && !modelPuntajeProducto.getIdUsuario().equals(idUser)) {
+                                        bandera = false;
+                                        break;
+                                    }
+                                }
 
-                                                    ModelPuntajeProducto modelPuntajeProducto1 = new ModelPuntajeProducto(idPro,idUser,puntaje);
-                                                    databaseReferencePuntaje.child(modelPuntajeProducto.getIdPuntaje()).setValue(modelPuntajeProducto1);
+                                databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
+                                databaseReferencePuntaje.orderByChild("idProducto").equalTo(idPro).addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if (snapshot.exists()) {
+                                            for (DataSnapshot oPuntaje : snapshot.getChildren()) {
+                                                ModelPuntajeProducto puntajeProducto = oPuntaje.getValue(ModelPuntajeProducto.class);
+                                                String idPuntaje = oPuntaje.getKey();
+                                                String idProducto = puntajeProducto.getIdProducto();
+                                                String idUsuario = puntajeProducto.getIdUsuario();
+                                                double puntaje = puntajeProducto.getPuntajeProducto();
+                                                listPuntajeProducto.add(new ModelPuntajeProducto(idPuntaje, idProducto, idUsuario, puntaje));
+                                            }
+                                            for (ModelPuntajeProducto modelPuntajeProducto : listPuntajeProducto){
+                                                if (modelPuntajeProducto.getIdProducto().equals(idPro)){
+                                                    idPuntajeQryProducto = modelPuntajeProducto.getIdPuntaje();
+                                                    idUsuarioPuntajeQryProducto = modelPuntajeProducto.getIdPuntaje();
+                                                    idProductoPuntajeQryProducto = modelPuntajeProducto.getIdProducto();
                                                 }
                                             }
 
-                                            @Override
-                                            public void onCancelled(@NonNull DatabaseError error) {
-
+                                            for (ModelPuntajeProducto modelPuntajeProducto1 : listPuntajeProductoUsuario){
+                                                if (modelPuntajeProducto1.getIdUsuario().equals(idUser)){
+                                                    idPuntajeQryUsuario = modelPuntajeProducto1.getIdPuntaje();
+                                                    idUsuarioPuntajeQryUsuario = modelPuntajeProducto1.getIdPuntaje();
+                                                    idProductoPuntajeQryUsuario = modelPuntajeProducto1.getIdProducto();
+                                                }
                                             }
-                                        });
+
+                                            if (idPuntajeQryProducto.equals(idPuntajeQryUsuario)) {
+                                                databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
+                                                double puntaje = ratingBar.getRating();
+                                                ModelPuntajeProducto modelPuntajeProducto1 = new ModelPuntajeProducto(idPro,idUser,puntaje);
+                                                databaseReferencePuntaje.child(idPuntajeQryProducto).setValue(modelPuntajeProducto1);
+                                            }
+                                        }
                                     }
-                                }
-                                String s = String.valueOf(ratingBar.getRating());
-                                textRating.setText(s);
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
                             }else{
-                                String idTalla = databaseReferencePuntaje.push().getKey();
-                                if(idTalla != null){
-                                    ModelPuntajeProducto modelPuntajeProducto = new ModelPuntajeProducto(idPro,idUser,ratingBar.getRating());
-                                    databaseReferencePuntaje.child(idTalla).setValue(modelPuntajeProducto);
-                                    String s = String.valueOf(ratingBar.getRating());
-                                    textRating.setText(s);
+                                databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
+                                double puntaje = ratingBar.getRating();
+                                try {
+                                    ModelPuntajeProducto modelPuntajeProducto3 = new ModelPuntajeProducto(idPro,idUser,puntaje);
+                                    String modelId = databaseReferencePuntaje.push().getKey();
+                                    if(modelId != null){
+                                        databaseReferencePuntaje.child(modelId).setValue(modelPuntajeProducto3);
+                                    }
+                                }catch (Exception e){
+                                    e.getStackTrace();
                                 }
                             }
                         }
@@ -269,9 +314,25 @@ public class DetalleActivity extends AppCompatActivity {
 
                         }
                     });
+
+                    if (!bandera){
+                        databaseReferencePuntaje = FirebaseDatabase.getInstance().getReference("PuntajeProducto");
+                        double puntaje = ratingBar.getRating();
+                        try {
+                            ModelPuntajeProducto modelPuntajeProducto3 = new ModelPuntajeProducto(idPro,idUser,puntaje);
+                            String modelId = databaseReferencePuntaje.push().getKey();
+                            if(modelId != null){
+                                databaseReferencePuntaje.child(modelId).setValue(modelPuntajeProducto3);
+                            }
+                        }catch (Exception e){
+                            e.getStackTrace();
+                        }
+                        bandera = true;
+                    }
                 }
             }
         });
+
 
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -410,5 +471,53 @@ public class DetalleActivity extends AppCompatActivity {
 
     public void setIdCarrito(String idCarrito) {
         this.idCarrito = idCarrito;
+    }
+
+    public String getIdProductoPuntajeQryUsuario() {
+        return idProductoPuntajeQryUsuario;
+    }
+
+    public void setIdProductoPuntajeQryUsuario(String idProductoPuntajeQryUsuario) {
+        this.idProductoPuntajeQryUsuario = idProductoPuntajeQryUsuario;
+    }
+
+    public String getIdUsuarioPuntajeQryUsuario() {
+        return idUsuarioPuntajeQryUsuario;
+    }
+
+    public void setIdUsuarioPuntajeQryUsuario(String idUsuarioPuntajeQryUsuario) {
+        this.idUsuarioPuntajeQryUsuario = idUsuarioPuntajeQryUsuario;
+    }
+
+    public String getIdProductoPuntajeQryProducto() {
+        return idProductoPuntajeQryProducto;
+    }
+
+    public void setIdProductoPuntajeQryProducto(String idProductoPuntajeQryProducto) {
+        this.idProductoPuntajeQryProducto = idProductoPuntajeQryProducto;
+    }
+
+    public String getIdUsuarioPuntajeQryProducto() {
+        return idUsuarioPuntajeQryProducto;
+    }
+
+    public void setIdUsuarioPuntajeQryProducto(String idUsuarioPuntajeQryProducto) {
+        this.idUsuarioPuntajeQryProducto = idUsuarioPuntajeQryProducto;
+    }
+
+    public String getIdPuntajeQryProducto() {
+        return idPuntajeQryProducto;
+    }
+
+    public void setIdPuntajeQryProducto(String idPuntajeQryProducto) {
+        this.idPuntajeQryProducto = idPuntajeQryProducto;
+    }
+
+    public String getIdPuntajeQryUsuario() {
+        return idPuntajeQryUsuario;
+    }
+
+    public void setIdPuntajeQryUsuario(String idPuntajeQryUsuario) {
+        this.idPuntajeQryUsuario = idPuntajeQryUsuario;
     }
 }
